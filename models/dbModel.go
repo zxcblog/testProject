11dfb57cc45f4f33908b82ffb9d9391d650460f5
recruit <zxc_7310@163.com>
2022-02-26 20:59:43 +0800

    添加用户表

--- models/dbModel.go
+++ models/dbModel.go
@@ -6,10 +6,22 @@
 //
 package models
 
+import "database/sql"
+
 var Models = []interface{}{
-	&Product{},
+	&Product{}, &User{},
 }
 
 type Product struct {
 	Model
+}
+
+type User struct {
+	Model
+	Status   uint           `gorm:"type:tinyint(1);index:idx_shop_user_status;not null"` // 状态
+	Username sql.NullString `gorm:"size:32;unique"`                                      // 账号
+	Nickname string         `gorm:"size:16"`                                             // 昵称
+	Avatar   string         `gorm:"type:text"`                                           // 头像
+	Password string         `gorm:"size:512"`                                            // 密码
+	Salt     string         `gorm:"size:16"`                                             // 密码加盐
 }
